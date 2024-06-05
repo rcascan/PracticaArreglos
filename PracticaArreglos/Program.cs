c127083f1f068b3b50343a9dffc4ba5a1da76167
rcascan <cascanteflores@gmail.com>
2024-06-04 20:56:39 -0600

    Agregar archivos de proyecto.

---
+++ PracticaArreglos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaArreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nombre = { "Luis", "Maria", "Karla", "Juan " };
+            string[] apellido = new string[3];
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine(nombre[i]);
+
+                //string parte = string.Join("-", nombre[i].ToCharArray());
+                //Console.WriteLine(parte);
+            }
+
+            //Console.WriteLine(nombre.Length);
+            Console.Read();
+
+        }
+    }
+}
